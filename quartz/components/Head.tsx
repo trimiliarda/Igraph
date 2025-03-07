81d6db10119fed592ff68787eff9e956f08e92a2
andfed <a.fedorov@rdpin.ru>
2025-03-07 11:51:49 +0300

    Quartz sync: Mar 7, 2025, 11:51 AM

--- quartz/components/Head.tsx
+++ quartz/components/Head.tsx
@@ -131,7 +131,8 @@ export default (() => {
 
     const { css, js, additionalHead } = externalResources
 
-    const url = new URL(`https://${cfg.baseUrl ?? "example.com"}`)
+    // const url = new URL(`https://${cfg.baseUrl ?? "example.com"}`)
+    const url = new URL('https://github.com/trimiliarda/Igraph/tree/v4/quartz')
     const path = url.pathname as FullSlug
     const baseDir = fileData.slug === "404" ? path : pathToRoot(fileData.slug!)
 
